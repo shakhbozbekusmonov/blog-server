0969b234ece1b749e698c267465b746d6c657a8c
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-01-06 00:11:46 +0500

    Deploy to the server configuration

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
